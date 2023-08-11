9fc76aff383d13c565c1f75f66efa7737a1601d2
Ohanele Michael <ohanelem@gmail.com>
2023-08-11 13:51:02 -0700

    printing without printf and puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * main - It is the entry point of a program.
+ *
+ * Return: The return statement is used to end the execution of a function.
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+
+}
