92f42ee5530ecb1410a1f5664398f42902a855dd
Ohanele Michael <ohanelem@gmail.com>
2023-08-11 09:38:55 -0700

    My first C program in Alx

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - It is the entry point of a program where the execution of a program starts.
+ *
+ * Return: The return statement is used to end the execution of a function and returns control to the function from where it was called
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+}
