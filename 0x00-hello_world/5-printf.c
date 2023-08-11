4b7757e392bf2a7cdf327fa98916a11d86fd6f1e
Ohanele Michael <ohanelem@gmail.com>
2023-08-11 09:56:10 -0700

    using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - It is the entry point of a program.
+ *
+ * Return: The return statement is used to end the execution of a function.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
