8e64365fb1f23ab2e4da113639f065ecccd430c8
Ohanele Michael <ohanelem@gmail.com>
2023-08-14 00:28:12 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - It is the entry point of a program.
+ *
+ * Return: The return statement is used to end the execution of a function.
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+
+}
+
