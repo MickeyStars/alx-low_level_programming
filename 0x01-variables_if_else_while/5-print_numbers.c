a219200e7a5be6ad771bcd47b5da8972768ee3d8
Ohanele Michael <ohanelem@gmail.com>
2023-08-14 13:04:22 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - It is the entry point of a program.
+ *
+ * Return: The return statement is used to end the execution of a function.
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+
+}
