0009f162ebd825459ea49fefdfb0fba170cfe89c
Ohanele Michael <ohanelem@gmail.com>
2023-08-14 04:07:12 -0700

    Alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+/**
+ * main - It is the entry point of a program.
+ *
+ * Return: The return statement is used to end the execution of a function.
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	for (i = 'A'; i <= 'Z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+
+}
