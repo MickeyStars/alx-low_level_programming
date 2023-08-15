2fb43998a2bdfa137dd1fdfeeaa2fa068f79779c
Ohanele Michael <ohanelem@gmail.com>
2023-08-14 17:37:23 -0700

    printing numbers with commas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - It is the entry point of a program.
+ *
+ * Return: The return statement is used to end the execution of a function.
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
