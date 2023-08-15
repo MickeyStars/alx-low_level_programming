79bb2af4f36ad526deb3703f29bf58e832c83f4c
Ohanele Michael <ohanelem@gmail.com>
2023-08-14 18:15:10 -0700

    task 9

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,18 +8,24 @@
 
 int main(void)
 {
-	int i;
+        int d,p;
 
-	for (i = 0; i < 10; i++)
-	{
-		putchar(i + '0');
-		if (i != 9)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
-	putchar('\n');
-	return (0);
+        for (d = 0; d < 9; d++)
+        {
+                for (p = d + 1; p <= 9; p++)
+                {
+                        if (p != d)
+                {
+                        putchar(d);
+                        putchar(p);
+                if (d == 8 && p == 9)
+                        continue;
+                putchar(',');
+                putchar(' ');
+                }
+                }
+        }
+        putchar('\n');
+        return (0);
 
 }
