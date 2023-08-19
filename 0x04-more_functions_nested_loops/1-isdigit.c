410c89bc65513637c0c7040aa849b88f0cd58168
Ohanele Michael <ohanelem@gmail.com>
2023-08-18 17:06:46 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,13 +2,12 @@
 
 /**
  *_isdigit - function that checks for 0 through 9
- * @C: parameter to be checked 
+ * @c: parameter to be checked
  * Return: 1 on sucess and 0 on error
-*/
+ */
 
 int _isdigit(int c)
 {
-
 	if (c >= 48 && c <= 57)
 		return (1);
 	else
