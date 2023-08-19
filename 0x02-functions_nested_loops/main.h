e71ce680f3e1acaf512cde045dca993230758645
Ohanele Michael <ohanelem@gmail.com>
2023-08-18 19:55:05 -0700

    gg

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,13 @@
-int_putchar(int c);
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(int c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
+int _putchar(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
@@ -10,3 +16,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+
+
+#endif /* MAIN_H */
