15ee0d4cbcf5297d868f409363ea45ae81a9dde6
Ohanele Michael <ohanelem@gmail.com>
2023-08-22 13:14:09 -0700

    reverse strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - printing a string
+ * @s: string to be counted
+ * Return: void
+*/
+
+void print_rev(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+		count++;
+	for (i = count - 1 ; i >= 0 ; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+
+
+}
+
+
