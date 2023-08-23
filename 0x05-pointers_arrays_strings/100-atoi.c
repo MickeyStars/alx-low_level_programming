4e23a2074d7abc72080c8902bac70f474bcd2376
Ohanele Michael <ohanelem@gmail.com>
2023-08-23 10:55:27 -0700

    100-atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,6 +6,7 @@
  *
  * Return: the int converted from the string
  */
+
 int _atoi(char *s)
 {
 	int i, d, n, len, f, digit;
@@ -19,12 +20,10 @@ int _atoi(char *s)
 
 	while (s[len] != '\0')
 		len++;
-
 	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
 			++d;
-
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
@@ -38,9 +37,7 @@ int _atoi(char *s)
 		}
 		i++;
 	}
-
 	if (f == 0)
 		return (0);
-
 	return (n);
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 
-int _atoi(char *s)
+int _atoi(char *s);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
