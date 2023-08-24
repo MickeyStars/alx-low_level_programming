0c89afb3416c0bb722e5a0c2178edd3ce7a5a585
Ohanele Michael <ohanelem@gmail.com>
2023-08-24 03:07:24 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncat - string pointer
+ * @n: integer value
+ * @dest: destination string
+ * @src: source string
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len = strlen(dest);
+	int i;
+
+	for (i = 0; i < n && *src != '\0'; i++)
+	{
+		dest[len + i] = src[i];
+		src++;
+	}
+	dest[len + i] = '\0';
+	return (dest);
+
+}
